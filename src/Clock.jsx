d2cc42a4aa90be6843aa2cddfc55167950efa1d1
shades3002 <shades3002@gmail.com>
2017-07-19 18:33:02 -0400

    Adding Helper Methods with Ternary Expressions

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -22,6 +22,10 @@ class Clock extends Component {
         1000);
     }
 
+    leading0(num) {
+        return num < 10 ? '0' + num : num;
+    }
+
     getTimeUntil(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
         const seconds = Math.floor((time/1000) % 60);
@@ -36,10 +40,10 @@ class Clock extends Component {
 
         return (
             <div>
-                <div className="Clock-days">{ this.state.days } days</div>
-                <div className="Clock-hours">{ this.state.hours } hours</div>
-                <div className="Clock-minutes">{ this.state.minutes } minutes</div>
-                <div className="Clock-seconds">{ this.state.seconds } seconds</div>
+                <div className="Clock-days">{ this.leading0(this.state.days) } days</div>
+                <div className="Clock-hours">{ this.leading0(this.state.hours) } hours</div>
+                <div className="Clock-minutes">{ this.leading0(this.state.minutes) } minutes</div>
+                <div className="Clock-seconds">{ this.leading0(this.state.seconds) } seconds</div>
             </div>
         )
     }
