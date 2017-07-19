45586894c368c8d7678517ae46e1949a4f35b097
shades3002 <shades3002@gmail.com>
2017-07-19 17:07:29 -0400

    Introducing State

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2017'
+        };
+    }
+    
     render () {
         return (
             <div className="App">
-                <div className="App-title">Countdown to December 25, 2017</div>
+                <div className="App-title">Countdown to {this.state.deadline}</div>
                 <div>
                     <div className="Clock-days">14 days</div>
                     <div className="Clock-hours">30 hours</div>
