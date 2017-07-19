f5217cb38d3daf5b00d16bdb2e369c98ddf8528a
shades3002 <shades3002@gmail.com>
2017-07-19 16:42:11 -0400

    Creating Your First Component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App  extends Component {
+    render () {
+        return (
+            <div>Countdown Champ, App Comp</div>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-    <div>countdown champ</div>, document.getElementById('root')
+    <App />, document.getElementById('root')
 )
