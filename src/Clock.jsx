e11ce20b5744d4b739499679ec16d8291e88a0b1
shades3002 <shades3002@gmail.com>
2017-07-19 18:03:24 -0400

    Calculating Time Until

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -10,10 +10,23 @@ class Clock extends Component {
             minutes: 0,
             seconds: 0
         }
-
-        console.log('props', this.props);
     }
+
+    getTimeUntil(deadline) {
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        console.log('time', time);
+        const seconds = Math.floor((time/1000) % 60);
+        const minutes = Math.floor((time/1000/60) % 60);
+        const hours = Math.floor(time/(1000*60*60) % 24);
+        const days = Math.floor(time/(1000*60*60*24));
+
+        console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
+        this.setState({days: days});
+    }
+
     render() {
+        this.getTimeUntil(this.props.deadline);
+
         return (
             <div>
                 <div className="Clock-days">{ this.state.days } days</div>
